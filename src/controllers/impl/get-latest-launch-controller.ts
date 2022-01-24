75fcb978aa827dee3dfe99ee0460384ec7ee8918
andremiguel1 <andre.mig@gmail.com>
2022-01-24 10:22:14 -0300

    refact: Latest launch router

--- src/controllers/impl/get-latest-launch-controller.ts
+++ src/controllers/impl/get-latest-launch-controller.ts
@@ -8,10 +8,10 @@ export class GetLatestLaunchController implements Controller<Launch> {
   constructor(protected readonly httpClientHelper: HttpClientHelper<Launch>) {}
 
   public async handle(): Promise<Launch> {
-    console.log('Controller Handle: start');
+    console.log('Get Latest Launch Controller Handle: start');
     const { data } = await this.httpClientHelper.get<Launch>(URL_LAUNCH);
     const { id, date_local, details, flight_number, name } = data as Launch;
-    console.log('Controller Handle: finish');
+    console.log('Get Latest Launch Controller Handle: finish');
     return { id, date_local, details, flight_number, name };
   }
 }
--- src/routes/latest-launch-router.ts
+++ src/routes/latest-launch-router.ts
@@ -5,15 +5,15 @@ import { HttpClientHelper } from '../helper/http-client-helper';
 import { AxiosClientHelper } from '../helper/impl/axios-client-helper';
 import { Launch } from '../model/launch-model';
 
-const route = express.Router();
+const latestLaunchRouter = express.Router();
 
-route.get('/latest', async (req: Request, res: Response) => {
-  console.log('Launch router start');
+latestLaunchRouter.get('/latest', async (req: Request, res: Response) => {
+  console.log('Latest launch router: start');
   const helper: HttpClientHelper<Launch> = new AxiosClientHelper();
   const controller: Controller<Launch> = new GetLatestLaunchController(helper);
   const latestLaunch = await controller.handle();
-  console.log('Launch router finish');
+  console.log('Latest launch router: finish');
   return res.send(latestLaunch);
 });
 
-export { route as latestLaunchRouter };
+export { latestLaunchRouter };
